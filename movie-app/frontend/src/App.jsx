bba907e6b76c3b55e5bbe6b809086e519ba8ee51
mznawaz <162314578+mznawaz@users.noreply.github.com>
2025-06-01 00:21:34 +0300

    Update App.jsx

--- movie-app/frontend/src/App.jsx
+++ movie-app/frontend/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    fetch(process.env.REACT_APP_MOVIE_API_URL || "http://localhost:3001/movies")
+    fetch(process.env.REACT_APP_MOVIE_API_URL || "http://a5007a8954fb84713a962829f408f5bb-472749424.us-east-1.elb.amazonaws.com/:3001/movies")
       .then((res) => res.json())
       .then(setMovies)
       .catch(console.error);
