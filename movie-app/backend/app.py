aa62af6e664fba36c68bb379bda4d80cf4d21476
mznawaz <162314578+mznawaz@users.noreply.github.com>
2025-06-02 10:53:03 +0300

    Create app.py

---
+++ movie-app/backend/app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/movies')
+def get_movies():
+    return jsonify([
+        {"id": 1, "title": "Inception"},
+        {"id": 2, "title": "The Matrix"},
+        {"id": 3, "title": "Interstellar"},
+        {"id": 4, "title": "Talaash"}
+      
+    ])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
