387f279439d4cd1eba0d0069dace54e23162dbd6
mznawaz <162314578+mznawaz@users.noreply.github.com>
2025-06-04 17:09:07 +0300

    Update app.py

--- movie-app/backend/app.py
+++ movie-app/backend/app.py
@@ -9,10 +9,10 @@ CORS(app, origins=["http://aa3452b66f0bb4a5b9a809d9b95a0448-930219611.us-east-1.
 @app.route('/movies')
 def get_movies():
     return jsonify([
-        {"id": 1, "title": "Inception"},
-        {"id": 2, "title": "The Matrix"},
-        {"id": 3, "title": "Interstellar"},
-        {"id": 4, "title": "Talaash"}
+        {"id": 1, "title": "Inception", "year": 2010},
+        {"id": 2, "title": "The Matrix", "year": 1999},
+        {"id": 3, "title": "Interstellar", "year": 2014},
+        {"id": 4, "title": "Talaash", "year": 2019}
     ])
 
 if __name__ == '__main__':
