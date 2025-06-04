7444e45bc093dd79e478f516aea7640f31027d7f
mznawaz <162314578+mznawaz@users.noreply.github.com>
2025-06-04 17:14:49 +0300

    Update app.py

--- movie-app/backend/app.py
+++ movie-app/backend/app.py
@@ -3,8 +3,13 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-# Enable CORS for your frontend origin
-CORS(app, origins=["http://aa3452b66f0bb4a5b9a809d9b95a0448-930219611.us-east-1.elb.amazonaws.com"])
+CORS(
+    app,
+    origins=[
+        "http://aa3452b66f0bb4a5b9a809d9b95a0448-930219611.us-east-1.elb.amazonaws.com"
+    ]
+)
+
 
 @app.route('/movies')
 def get_movies():
@@ -12,8 +17,9 @@ def get_movies():
         {"id": 1, "title": "Inception", "year": 2010},
         {"id": 2, "title": "The Matrix", "year": 1999},
         {"id": 3, "title": "Interstellar", "year": 2014},
-        {"id": 4, "title": "Talaash", "year": 2019}
+        {"id": 4, "title": "Talaash", "year": 2012}
     ])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3001)
